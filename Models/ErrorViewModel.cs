0384a0162b659315193bd3037657913126161336
Emmet O Neill <emmetoneill18@gmail.com>
2025-08-15 22:02:08 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Frameworks_2025_EON.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
